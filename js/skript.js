1ee0c3cfab96d5c50abac1b008330c3fbf27270b
Vladislav Zmaga <vladislavzmaga@gmail.com>
2022-07-18 02:23:56 +0300

    фвв

--- js/skript.js
+++ js/skript.js
@@ -1,7 +1,16 @@
 let a = prompt("Ти будеш блять учить JavaScript?  Напиши да або ні і нажми ок");
-if (a === "да" || a === "yes") {
+if (
+  a.toLowerCase() === "да" ||
+  a.toLowerCase() === "yes" ||
+  a.toLowerCase() === "ok"
+) {
   alert("красава, напиши своєму братові він тобі поможе))))");
-} else if (a === "no" || a === "not" || a === "нет" || a === "ні") {
+} else if (
+  a.toLowerCase() === "no" ||
+  a.toLowerCase() === "not" ||
+  a.toLowerCase() === "нет" ||
+  a.toLowerCase() === "ні"
+) {
   alert(
     "Все з тобой понятно(((, ну і оставайся необразованим клубникозбирачем)))"
   );
